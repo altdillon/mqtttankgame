a8c01f7b2e56ad0774bbd1c8886da1d286cf928e
Dillon <Dillon>
2025-09-24 23:01:16 -0600

    progress on init badguy tank

--- badguytank.c
+++ badguytank.c
@@ -2,6 +2,10 @@
 
 int init_tanks(mqtthost_t *mqtthosts,badguytank_h *badguys,uint32_t ntanks)
 {
+    for(uint32_t i=0;i<ntanks;i++)
+    {
+        badguys[i].currentstate = PATROL;
+    }
 
     return 0;
 }
