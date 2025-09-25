9a410c11ade52bef38acb68d8d870740c64b8dc7
Dillon <Dillon>
2025-09-24 23:02:24 -0600

    minor clean up

--- badguytank.c
+++ badguytank.c
@@ -5,6 +5,7 @@ int init_tanks(mqtthost_t *mqtthosts,badguytank_h *badguys,uint32_t ntanks)
     for(uint32_t i=0;i<ntanks;i++)
     {
         badguys[i].currentstate = PATROL;
+        
     }
 
     return 0;
