1dd2908e59d68656deae99bf724e9b8187fbdf2a
Dillon <Dillon>
2025-09-24 22:53:36 -0600

    progress on badguytank and created badguytank.c

--- badguytank.h
+++ badguytank.h
@@ -1,5 +1,9 @@
 #ifndef BADGUY_H
 #define BADGUY_H
+#define MAXBADGUYTANKS 20 // max number of bad guy tanks
+#include <stdint.h>
+#include "raylib.h"
+#include "mqtthosts.h"
 
 /*
     enum for the operating state of the bad guy tank
@@ -14,7 +18,32 @@ enum tankstate
 };
 
 /*
-    header files for the bad guy tank
+    struct that stores the state of the current tank
 */
+typedef struct 
+{
+    Vector2 tankpos; // 2D position of the badguy tank
+    tankstate currentstate;
+} badguytank_h;
+
+/*
+    header prototypes for the bad guy tank
+*/
+
+/*
+    init the tank, start out each tank on patrol
+    mqtthosts: array of mqtt hosts from the toml file
+    badguys: array of badguy tanks
+    ntanks: number of tanks that we want to create
+    return 0 if everything word out, -1 if it didn't
+*/
+int init_tanks(mqtthost_t *mqtthosts,badguytank_h *badguys,uint32_t ntanks);
+
+/*
+    compute the next state of a tank and update the memory
+    tank: a pointer to a tank object
+*/
+
+void tank_nextate(badguytank_h *tank);
 
 #endif
